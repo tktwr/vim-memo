da40ac7d4c45101261bc5b86abe2f66bf3778a0c
tktwr <takehiro.tawara@gmail.com>
2023-03-20 17:22:00 +0900

    update memotags.sh

--- bin/memotags.sh
+++ bin/memotags.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 f_memotags() {
-  echo "!_TAG_FILE_ENCODING	utf-8	//"
+  echo '!_TAG_FILE_ENCODING	utf-8	//'
   word=$1
   shift
   for i in "$@"; do
     tags=`grep "*$word" $i | sed -n "s/.*\*\($word.*\)\*.*/\1/p"`
     for j in $tags; do
-      echo "$j	$i	/*$j*"
+      echo "$j	$i	/*$j*/;\"	f"
     done
   done
 }
